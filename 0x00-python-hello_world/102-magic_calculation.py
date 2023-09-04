09956325c391d66a724747a03b7805d71ddf8dd3
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-04 08:03:56 -0700

    hello

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,9 +1,4 @@
-import dis
-
 def magic_calculation(a, b):
     result = 98
     result += a ** b
     return result
-
-# Display the bytecode of the magic_calculation function
-dis.dis(magic_calculation)
