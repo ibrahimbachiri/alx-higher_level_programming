33b441fd5c2480645c8e783a77b0f90660cd81ff
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-05 01:34:19 -0700

    hello

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[0:11] + str[38:64])
+print("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[0:11] + " " + "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"[38:64])
