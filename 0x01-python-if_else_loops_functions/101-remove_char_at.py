6dfd99b989925de8eec0ecca9399de6d420f78ac
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-05 02:30:18 -0700

    if

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,13 +2,10 @@
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
-
     return str[:n] + str[n+1:]
-
-# Test the remove_char_at function
 if __name__ == "__main__":
-    print(remove_char_at("Chicago", 3))  # Correct output - case: remove_char_at("Chicago", 3)
-    print(remove_char_at("Chicago", 15))  # Correct output - case: remove_char_at("Chicago", 15)
-    print(remove_char_at("", 4))  # Correct output - case: remove_char_at("", 4)
-    print(remove_char_at("Chicago", -3))  # Correct output - case: remove_char_at("Chicago", -3)
-    print(remove_char_at("Chicago", 0))  # Correct output - case: remove_char_at("Chicago", 0)
+    print(remove_char_at("Chicago", 3))
+    print(remove_char_at("Chicago", 15))
+    print(remove_char_at("", 4))
+    print(remove_char_at("Chicago", -3))
+    print(remove_char_at("Chicago", 0))
