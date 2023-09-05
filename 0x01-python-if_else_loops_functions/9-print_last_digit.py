3f95422aad003e628043e55fd27b4580ff0d268e
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-05 04:41:05 -0700

    if

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the last digit using modulo
-    print(last_digit, end='')  # Print the last digit without a newline
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
     return last_digit
 
-# Two blank lines here
 
-# Test cases
 if __name__ == "__main__":
     print_last_digit(98)
     print_last_digit(0)
