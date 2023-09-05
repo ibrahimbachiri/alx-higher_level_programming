6cdf05e2fd57f4b3cc99de9916e8b011f2c42c50
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-05 03:24:03 -0700

    if

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 
 for letter in range(ord('a'), ord('z') + 1):
     if letter != ord('e') and letter != ord('q'):
-        print(chr(letter), end="")
+        print("{}".format(chr(letter)), end="")
