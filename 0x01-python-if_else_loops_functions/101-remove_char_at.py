25cd541a78310dff7059100363d212f782b50838
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-05 02:58:16 -0700

    if

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
 
+def remove_char_at(str, n):
     if n < 0 or n >= len(str):
-    return str
+        return str
+
     return str[:n] + str[n+1:]
 
 if __name__ == "__main__":
