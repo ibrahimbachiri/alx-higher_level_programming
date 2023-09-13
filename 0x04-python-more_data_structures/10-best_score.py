09362d57f79094d161a203d2f7300125d34fae4d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-13 10:01:57 -0700

    more

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        # Use the max() function with a custom key function to find the key with the largest value
         best_key = max(a_dictionary, key=lambda k: a_dictionary[k])
         return best_key
     else:
