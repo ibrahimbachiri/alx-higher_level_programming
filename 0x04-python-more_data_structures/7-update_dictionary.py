c9917d4d025f43284dfd0a627c370c409a27bbc3
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-13 09:34:39 -0700

    more

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # Use the assignment operator to add or update the key-value pair
+    a_dictionary[key] = value
+    return a_dictionary
