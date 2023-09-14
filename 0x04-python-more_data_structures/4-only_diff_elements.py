c2ff604d86cfe3df6657bffb8e70b48b923e6bfb
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-14 07:18:18 -0700

    more

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):    
+def only_diff_elements(set_1, set_2):
+    # Use the intersection operation (&) to find common elements
     diff_set = set_1 ^ set_2
     return diff_set
