ce7e3516cccb537cb91669d42e14a320d07949d6
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-14 04:23:17 -0700

    more

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,11 +4,8 @@ def weight_average(my_list=[]):
         return 0.0
 
     
-    # Initialize variables to store the sum of products and the sum of weights
     sum_products = 0
     sum_weights = 0
-
-    # Iterate through the tuples in the list and calculate the sum of products and weights
     for score, weight in my_list:
         sum_products += score * weight
         sum_weights += weight
