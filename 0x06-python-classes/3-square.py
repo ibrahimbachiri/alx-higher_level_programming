ee308fedbe8214f916dc40012c2b7046496ac003
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-26 05:21:52 -0700

    m

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Represent a class."""
+
+
 class Square:
+    """Define a size."""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
