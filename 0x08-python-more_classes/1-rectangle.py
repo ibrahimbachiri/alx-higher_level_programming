178df5b3af530eacf0e3614435425c5b42d7ca9d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-02 06:05:13 -0700

    m

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Represent a class."""
+"""
+This module defines the a Rectangle Object.
+"""
 
 
 class Rectangle:
-    """Define init."""
-
-    
+    """Retangle object with getter and setters
+    """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
