5a7345eec488a476d8bb9bfb3512b6a4bf874f7d
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-02 15:52:20 -0700

    m

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -4,9 +4,10 @@
 
 class Rectangle:
     """Define a Rectangle."""
+    
     number_of_instances = 0
     print_symbol = "#"
-
+    
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
