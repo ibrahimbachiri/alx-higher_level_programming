9614b5aa103dba2be304b01239dfc560a4bcc699
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-04 03:59:30 -0700

    m

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Represent a matrix."""
+
+
 def matrix_mul(m_a, m_b):
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("m_a must be a list or m_b must be a list")
