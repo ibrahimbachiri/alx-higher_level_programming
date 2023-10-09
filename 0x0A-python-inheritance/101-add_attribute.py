449d23122260863da234d4ba040fc54fe7c51fb5
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-09 08:59:40 -0700

    m

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
-
 """Define an attribute."""
+
+
 def add_attribute(obj, attribute, value):
+    """Define an attribute."""
+
     if not hasattr(obj, attribute):
 
         setattr(obj, attribute, value)
