aa6802f6e6b86127325156d341447def8ef77c45
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-09 10:09:58 -0700

    m

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""Definition of an object."""
 
 
 def lookup(obj):
+    """
+    Retrieve and return the list of attributes and methodes of an object.
+    Args:
+        obj: The object to look up.
+
+    Returns:
+        list: A list of strings containing attribute and method names.
+    """
     return dir(obj)
 
-"""Definition of a class."""
 class MyClass1(object):
+    """
+    Definition of MyClass1.
+
+    This is a simple class with no attributes or methods.
+    """
     pass
 
-"""Definition of a class."""
 class MyClass2(object):
+    """
+    Definition of Myclass2.
+
+    This class has an attribute 'my_attr1' and a method 'my_meth'.
+    """
+
     my_attr1 = 3
 
 def my_meth(self):
+    """A placeholder method."""
     pass
 
 print(lookup(MyClass1))
