c6dc54714b7b2d4953e56e9a93637aa9941b5eed
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-09 07:31:11 -0700

    m

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,6 @@ class MyInt(int):
     """class that inherits from int"""
 
     def __init__(self, value):
-        """for initialiaze a value my init."""
 
         super().__init__()
         self.value = value
