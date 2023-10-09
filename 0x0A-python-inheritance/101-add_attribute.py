f595e00ec8e03fe0211ce6b77c47be6c10b2412b
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-09 08:50:03 -0700

    m

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_attribute(obj, attribute, value):
+    if not hasattr(obj, attribute):
+
+        setattr(obj, attribute, value)
+
+    else:
+        raise TypeError("can't add new attribute")
+
+if __name__ == "__main__":
+    class MyClass():
+        pass
+
+        mc = MyClass()
+        add_attribute(mc, "name", "John")
+        print(mc.name)
+
+    try:
+        a = "My String"
+        add_attribute(a, "name", "Bob")
+        print(a.name)
+
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
