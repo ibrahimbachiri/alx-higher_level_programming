3cca70d5346f8a747a182a4b141270e18043382c
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-09 09:33:28 -0700

    m

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
+"""Define an attribute."""
+
 
 def add_attribute(obj, attribute, value):
-    """
-    Define an attribute for an object if it doesn't already exist.
+    """Define an attribute."""
 
-    Args:
-        obj: The object to which the attribute should be added.
-        attribute: The name of the attribute.
-        value: The value of the attribute.
+    if not hasattr(obj, attribute):
 
-    Raises:
-        TypeError: If the attribute already exists on the object.
-    """
-    if not hasattr(obj, attribute):     
         setattr(obj, attribute, value)
+
     else:
-        raise TypeError("Can't add a new attribute")
-
-
-class MyClass:
-    pass
+        raise TypeError("can't add new attribute")
 
 if __name__ == "__main__":
-    mc = MyClass()
-    add_attribute(mc, "name", "John")
-    print(mc.name)
-    
+
+    """Represent a class."""
+    class MyClass():
+        pass
+
+        mc = MyClass()
+        add_attribute(mc, "name", "John")
+        print(mc.name)
+
     try:
         a = "My String"
-        add_attribute(a, "name", "Bob")  
+        add_attribute(a, "name", "Bob")
         print(a.name)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
