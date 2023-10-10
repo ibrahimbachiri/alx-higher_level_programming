2cc1dbb28c488e5966d550b46bff1c6b348691e9
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-10 05:36:09 -0700

    m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads filename with utf-8"""
-         with open(filename, encoding='utf-8') as f:
-    print(f.read(), end='')
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end='')
