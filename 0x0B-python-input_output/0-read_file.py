c418b49d9e0a5b27de9daea007fd32acfad9d9ac
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-10 04:15:20 -0700

    m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Definition of afile."""
-
-
 def read_file(filename=""):
-        with open(filename, mode='r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
+    with open(filename, mode='r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
 
 if __name__ == "__main__":
     read_file("my_file_0.txt")
