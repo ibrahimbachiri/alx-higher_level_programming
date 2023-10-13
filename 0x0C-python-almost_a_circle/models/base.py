3014b1ce1f7e31717644c9b639d5993a8bfbd992
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-13 10:34:08 -0700

    m

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,26 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs:
+            '''jsonified object to file.'''
+            if list_objs is not None:
+                list_objs = [o.to_dictionary() for o in list_objs]
+            with open("{}.json".format(cls.__name__),"w", encoding="utf-8")as f:
+
+
