7a15c49cd8685f28534a73d397bb5a0f27c60269
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-10-28 06:02:20 -0700

    m

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""defining to_json_string function"""
+"""defining to_json_string function."""
+
+
 def append_write(filename="", text=""):
     """Appends a string to a text file (UTF-8) and returns the number of characters added."""
     with open(filename, mode='a', encoding='utf-8') as file:
