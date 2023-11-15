db6b770d14bf8ad7a762c90f6d899f1b44ae0156
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-11-15 09:17:20 -0800

    m

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,6 @@
 -- use left JOIN and subquery
 -- Execute: cat 101-not_a_comedy.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
-SELECT title 
+SELECT title
 FROM tv_shows
 WHERE title NOT IN (SELECT tv_shows.title
 	FROM tv_shows
@@ -8,5 +8,6 @@ WHERE title NOT IN (SELECT tv_shows.title
 	ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
-	WHERE tv_genres.name = 'Comedy')
+	WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY 1 ASC;
