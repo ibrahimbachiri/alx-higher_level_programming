988b12f20ff684f421d721e04cae43013cd57fac
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-11-15 07:23:14 -0800

    m

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Print the average score
--- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT AVG(score) AS average FROM second_table;
+-- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c0
+SELECT AVG(score) AS average FROM scond_table;
